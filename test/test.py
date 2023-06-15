233fe800312ac3e36a8464f4977394137e2984b5
Begir72 <benjamin-girard@hotmail.ca>
2023-06-15 15:55:23 -0400

    work in progress: apply the perfect curve

--- test/test.py
+++ test/test.py
@@ -1,9 +1,40 @@
-def func(x, *y):
-    if not y:
-        print('gg')
-    
-    else:
-        print(x)
+from lmfit import Parameters, minimize
 
 
-func(1, 8)
+
+order = 9
+
+
+
+def polynomial_func(*args):
+
+    if isinstance(args[0], list):
+        args = args[0]
+
+    x = args[0]
+    result = 0
+
+    for pos, value in enumerate(args[1:]):
+        result += value*x**pos
+
+    return result
+
+
+
+p2 = Parameters()
+for i in range(order+1):
+    p2.add(chr(97+i), value=1)
+
+
+
+def residual2(pars, x, data):
+    params_list = []
+    for i in range(order+1):
+        params_list.append(pars[chr(97+i)])
+
+    model = polynomial_func(x, params_list)
+    return model - data
+
+for i in range(256):
+
+    print(chr(i))
