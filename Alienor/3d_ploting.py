593ec1d002a06a8c738ac9ae368e140917070b1a
Begir72 <benjamin-girard@hotmail.ca>
2023-08-02 10:28:36 -0400

    update ali

--- Alienor/3d_ploting.py
+++ Alienor/3d_ploting.py
@@ -1,59 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
 from matplotlib.ticker import MaxNLocator
 from matplotlib import cm
 from lmfit import Parameters, minimize
 
 
+
+# Met le chemin pour accéder au fichier là
+#                   ↓↓↓
 with open(r'Alienor\FreeformAnalysis.txt') as file:
     data = np.loadtxt(file, delimiter='\t')
     file.close()
 
-cut = 20
+# Tu peux augmenter le bond pour que ça bug moins
+bond = 20
 
-x = data[::cut,0]
-y = data[::cut,1]
-z = data[::cut,2]
+x = data[::bond,0]
+y = data[::bond,1]
+z = data[::bond,2]
 
 xy = np.concatenate(([x], [y]), axis=0)
 
 
-def func(xy, a, b, c, d, e):
-    #print(len(xy))
+def func(xy, a, b, c, d):
     x, y = xy
-    return a + (x/b + c)**2 + (y/d + e)**2
+    return -a + (x - b)**2/(4*d) + (y - c)**2/(4*d)
 
 p = Parameters()
-p.add('a', value=-5.28)
-p.add('b', value=63.93)
-p.add('c', value=-3.86)
-p.add('d', value=60.15)
-p.add('e', value=-1.61)
+p.add('a', value=5)
+p.add('b', value=240)
+p.add('c', value=100)
+p.add('d', value=1000)
 
 def residual(pars, xy, data):
     a = pars['a']
     b = pars['b']
     c = pars['c']
     d = pars['d']
-    e = pars['e']
-    model = func(xy, a, b, c, d, e)
+    model = func(xy, a, b, c, d)
     return model - data
 
+
+
+
 out = minimize(residual, p, args=(xy, z))
-#print(out.last_internal_values)
+
+print(out.last_internal_values)
+
+
+moy = (np.mean(out.residual))
+
+RMS = np.sqrt(np.sum(np.square(np.absolute(z))))/len(z)
+print('RMS =', RMS)
+
+PV = max(z)-min(z)
+print('PV =', PV)
+
 
 
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
+#surf = ax.plot_trisurf(x, y, z, cmap=cm.jet, linewidth=0)
 surf = ax.plot_trisurf(x, y, out.residual, cmap=cm.jet, linewidth=0)
 fig.colorbar(surf)
 
 ax.xaxis.set_major_locator(MaxNLocator(5))
 ax.yaxis.set_major_locator(MaxNLocator(6))
 ax.zaxis.set_major_locator(MaxNLocator(5))
+
+ax.set_xlabel('[mm]')
+ax.set_ylabel('[mm]')
 
 fig.tight_layout()
 
--- Alienor/test.py
+++ Alienor/test.py
@@ -1,21 +1,55 @@
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
 import numpy as np
-
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-# Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
-print(X)
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib import cm
+from lmfit import Parameters, minimize
 
 
-plt.show()
+
+# Met le chemin pour accéder au fichier là
+#                   ↓↓↓
+with open(r'Alienor\FreeformAnalysis.txt') as file:
+    data = np.loadtxt(file, delimiter='\t')
+    file.close()
+
+# Tu peux augmenter le bond pour que ça bug moins
+bond = 1
+
+res = np.empty(0)
+
+
+x = data[::bond,0]
+y = data[::bond,1]
+z = data[::bond,2]
+
+xy = np.concatenate(([x], [y]), axis=0)
+
+
+def func(xy, a, b, c, d):
+    x, y = xy
+    return -a + (x - b)**2/(4*d) + (y - c)**2/(4*d)
+
+p = Parameters()
+p.add('a', value=5)
+p.add('b', value=240)
+p.add('c', value=100)
+p.add('d', value=1000)
+
+def residual(pars, xy, data):
+    a = pars['a']
+    b = pars['b']
+    c = pars['c']
+    d = pars['d']
+    model = func(xy, a, b, c, d)
+    return model - data
+
+
+out = minimize(residual, p, args=(xy, z))
+
+np.savetxt('Erreur.txt', out.residual)
+
+
+    #RMS = np.sqrt(np.sum(np.square(out.residual)))/len(out.residual)
+
+    #res = np.append(res, RMS)
+
